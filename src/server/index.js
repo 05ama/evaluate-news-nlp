9e770ccb12f785fd63a1258708205c1193874d24
05ama <ausamamohsen@gmail.com>
2021-10-27 11:32:55 +0200

    fix: Fix server error

--- src/server/index.js
+++ src/server/index.js
@@ -8,7 +8,7 @@ const axios = require("axios");
 const cors = require("cors");
 
 const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+
 
 const app = express()
 
@@ -41,6 +41,4 @@ app.listen(8081, function () {
     console.log('Example app listening on port 8081!')
 })
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+
